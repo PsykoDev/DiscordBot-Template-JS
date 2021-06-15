9da448f702d87a1536b74c416daea1bc0b28563f
PsykoDev <45910905+PsykoDev@users.noreply.github.com>
2021-06-15 11:24:40 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+/** @format */
+
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const config = require("./config");
+const fs = require("fs");
+
+/* Config Music */
+client.queue = new Map();
+client.skipvote = new Map();
+
+for (let file of fs.readdirSync("./events/")) {
+	if (file.endsWith(".js")) {
+		let fileName = file.substring(0, file.length - 3);
+		let fileContents = require(`./events/${file}`);
+
+		client.on(fileName, fileContents.bind(null, client));
+
+		delete require.cache[require.resolve(`./events/${file}`)];
+	}
+}
+client.once("reconnecting", () => {
+	console.log("Reconnecting!");
+});
+client.once("disconnect", () => {
+	console.log("Disconnect!");
+});
+
+client.login(config.token);
